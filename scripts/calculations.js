60870b574ad523f63902bc5bfef4d3498b1d08b7
Luis <Cposada10@outlook.com>
2026-05-17 19:35:45 -0500

    Variables y datos prueba

---
+++ scripts/calculations.js
@@ -0,0 +1,18 @@
+// Primer presupuesto
+let budgetValue = 0;
+
+// Acumulados de gastos
+let totalExpensesValue = 0;
+
+// Color del estado
+let balanceColor = "green";
+
+let expenseEntries = [
+  ["groceries", 33],
+  ["restaurants", 50],
+  ["transport", 12],
+  ["home", 70],
+  ["subscriptions", 14],
+  ["groceries", 28],
+  ["subscriptions", 12],
+];
