51758be4a3ef6500ea07bafd975527420fce89d1
Luis <Cposada10@outlook.com>
2026-05-20 01:44:21 -0500

    Función calculateBalance

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -16,3 +16,21 @@ let expenseEntries = [
   ["groceries", 28],
   ["subscriptions", 12],
 ];
+
+function calculateBalance() {
+  totalExpensesValue = 0;
+
+  for (let i = 0; i < expenseEntries.length; i++) {
+    totalExpensesValue += expenseEntries[i][1];
+  }
+
+  let currentBalance = budgetValue - totalExpensesValue;
+
+  if (currentBalance < 0) {
+    balanceColor = "red";
+  } else if (currentBalance === 0) {
+    balanceColor = "yellow";
+  } else {
+    balanceColor = "green";
+  }
+}
